b948cd09fbe16e83ee59bfbc2627eb112f743594
avatar <avatar@IIT>
2011-01-05 18:55:59 +0200

    Add EC Compression to repo

---
+++ ec_compress.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+/* Compressed EC Points. For data transmission.  */
+
+#include "ec.hpp"
+
+class EC_CPoint
+{
+    const ZZ X;
+    const unsigned char tY;
+
+public:
+    
+    static size_t serializeSize(const unsigned char * data, size_t size);
+    
+    static inline ZZ deserialize_X(const unsigned char * data, size_t size)
+        {
+            return ZZFromBytes(data+1, serializeSize(data, size));
+        }
+    
+    static inline unsigned char deserialize_tY(const unsigned char * data, size_t size)
+        {
+            return ( data[size] & 0x1 );
+        }
+    
+    static inline unsigned char compress_tY(const EC_Point & X)
+        {
+            const ZZ & zzY = rep(X.getY());
+            return (unsigned char) bit(zzY, NumBits(zzY));
+        }
+    
+public:
+    EC_CPoint(const EC_Point & Point);
+    EC_CPoint(const ZZ_p & X, unsigned char tY);
+    EC_CPoint(const unsigned char * data, size_t size);
+    
+    ~EC_CPoint();
+    
+    EC_Point decompress(const EC & EC) const;
+
+    unsigned char * serialize(unsigned char * buffer,
+                              size_t size) const;
+
+    unsigned char * serialize(unsigned char * buffer,
+                              size_t size, const EC & EC) const;
+    
+    inline size_t serializeSize() const
+        { return NumBytes(X) + 1; }
+    
+    
+    bool operator== (EC_CPoint & Point) const;
+};
+
+    
