d5b32ad24bfbde586f5ff22a47f1ba8e966c463f
avatar <avatar@IIT>
2011-01-11 18:11:19 +0200

    Add testing loops

--- sign.cpp
+++ sign.cpp
@@ -28,97 +28,101 @@ int main(int argc     __attribute__((unused)),
     EC EC = EC_Defaults::create(EC_Defaults::EC160);
 
     EC_Defaults::restoreContext();
-    
-    
-    EC.enter_mod_context(EC::FIELD_CONTEXT);
+
+    for (unsigned int i = 0; i<20000; i++)
+    {
         
-    cout << "EC:N: " << I2OSP(EC.getOrder()) << endl;
-
-    const size_t Ln = L(EC.getOrder());
     
-    cout << "L_bits(N): " << Ln << endl;
+        EC.enter_mod_context(EC::FIELD_CONTEXT);
+        
+        //cout << "EC:N: " << I2OSP(EC.getOrder()) << endl;
+
+        const size_t Ln = L(EC.getOrder());
     
-    cout << "L(N): " << L(EC.getOrder()) << endl;
-
-    cout << "Current modulus: " << ZZ_p::modulus() << endl;
+        //cout << "L_bits(N): " << Ln << endl;
     
-    const ZZ_p Xa = ZZ_p_str("209173461442612814547828376902112692552984027342");
+        //cout << "L(N): " << L(EC.getOrder()) << endl;
+
+        //cout << "Current modulus: " << ZZ_p::modulus() << endl;
     
-    const EC_Point Y = EC.getBasePoint() * Xa;
-
-    cout << "Private key: " << I2OSP(Xa) << endl;
-    cout << "Public key: " << Y << endl;
-
-    const ZZ_p k = ZZ_p_str("49435060524598955092027863610721442198924735270");
-    const EC_Point kG = EC.getBasePoint() * k;
-
-    cout << "Session key: " << I2OSP(k) << endl;
-    cout << "Session radical: " << kG << endl;
-
-    const ByteSeq Pi = EC2OSP(kG,EC2OSP_COMPRESSED);
-
-    cout << "Π : " << Pi << endl;
-
-    string M("This is a test message!");
-
-    const long L_rec = 10;
-    const long L_red = 9;
-    const long L_clr = M.length() - L_rec;
+        const ZZ_p Xa = ZZ_p_str("209173461442612814547828376902112692552984027342");
     
-    cout << "Message: '" << M << "'" << endl;
-    cout << "[ L_rec: " << L_rec << "; L_clr: "
-         << L_clr << "; L_red: " << L_red << endl;
+        const EC_Point Y = EC.getBasePoint() * Xa;
 
-    const Octet   C_rec = I2OSP(L_rec, 4);
-    const Octet   C_clr = I2OSP(L_clr, 4);
+        //cout << "Private key: " << I2OSP(Xa) << endl;
+        //cout << "Public key: " << Y << endl;
 
-    cout << "C_rec: "  << C_rec << endl;
-    cout << "C_clr: "  << C_clr << endl;
+        const ZZ_p k = ZZ_p_str("49435060524598955092027863610721442198924735270");
+        const EC_Point kG = EC.getBasePoint() * k;
 
-    const ByteSeq M_rec = ByteSeq((const unsigned char *)
-                                  M.substr(0, L_rec).c_str(),
-                                  L_rec);
-    const ByteSeq M_clr = ByteSeq((const unsigned char *)
-                                  M.substr(L_rec, L_clr).c_str(),
-                                  L_clr);
+        //cout << "Session key: " << I2OSP(k) << endl;
+        //cout << "Session radical: " << kG << endl;
 
-    cout << "M_rec: "  << M_rec << endl;
-    cout << "M_clr: "  << M_clr << endl;
+        const ByteSeq Pi = EC2OSP(kG,EC2OSP_COMPRESSED);
 
-    Octet Hash_Input = C_rec || C_clr || M_rec || M_clr || Pi || C;
+        //cout << "Π : " << Pi << endl;
 
-    cout << "Hash Input: " << Hash_Input << endl;
+        string M("This is a test message!");
 
-    ByteSeq Hash_Token = Truncate(Hash(Hash_Input), L_red);
+        const long L_rec = 10;
+        const long L_red = 9;
+        const long L_clr = M.length() - L_rec;
     
-    cout << "Hash_Token: " << Hash_Token << endl;
+        //cout << "Message: '" << M << "'" << endl;
+        //cout << "[ L_rec: " << L_rec << "; L_clr: " << L_clr << "; L_red: " << L_red << endl;
 
-    ByteSeq D = Hash_Token || M_rec;
+        const Octet   C_rec = I2OSP(L_rec, 4);
+        const Octet   C_clr = I2OSP(L_clr, 4);
 
-    cout << "D: " << D << endl;
+        //cout << "C_rec: "  << C_rec << endl;
+        //cout << "C_clr: "  << C_clr << endl;
 
-    EC.enter_mod_context(EC::ORDER_CONTEXT);
+        const ByteSeq M_rec = ByteSeq((const unsigned char *)
+                                      M.substr(0, L_rec).c_str(),
+                                      L_rec);
+        const ByteSeq M_clr = ByteSeq((const unsigned char *)
+                                      M.substr(L_rec, L_clr).c_str(),
+                                      L_clr);
+
+        //cout << "M_rec: "  << M_rec << endl;
+        //cout << "M_clr: "  << M_clr << endl;
+
+        Octet Hash_Input = C_rec || C_clr || M_rec || M_clr || Pi || C;
+
+        //cout << "Hash Input: " << Hash_Input << endl;
+
+        ByteSeq Hash_Token = Truncate(Hash(Hash_Input), L_red);
     
-    const ZZ_p d = InMod(OS2IP(D));
-    const ZZ_p pi = InMod(OS2IP(Pi));
+        //cout << "Hash_Token: " << Hash_Token << endl;
 
-    cout << "d: " << d << endl;
-    cout << "π: " << pi << endl;
+        ByteSeq D = Hash_Token || M_rec;
+
+        //cout << "D: " << D << endl;
+
+        EC.enter_mod_context(EC::ORDER_CONTEXT);
     
-    const ZZ_p r = (d + pi);
-    const ZZ_p s = (k - Xa*r);
+        const ZZ_p d = InMod(OS2IP(D));
+        const ZZ_p pi = InMod(OS2IP(Pi));
 
-    cout << "r: " << r << endl;
-    cout << "s: " << s << endl;
+        //cout << "d: " << d << endl;
+        //cout << "π: " << pi << endl;
     
-    const ByteSeq R = I2OSP(r,Ln);
-    const ByteSeq S = I2OSP(s,Ln);
+        const ZZ_p r = (d + pi);
+        const ZZ_p s = (k - Xa*r);
 
-    cout << "R: " << R << endl;
-    cout << "S: " << S << endl;
+        //cout << "r: " << r << endl;
+        //cout << "s: " << s << endl;
+    
+        const ByteSeq R = I2OSP(r,Ln);
+        const ByteSeq S = I2OSP(s,Ln);
 
-    cout << "Current modulus: " << ZZ_p::modulus() << endl;
+        // cout << "R: " << R << endl;
+        // cout << "S: " << S << endl;
 
+        //cout << "Current modulus: " << ZZ_p::modulus() << endl;
+
+    }
+    
     EC.leave_mod_context();
     
     return 0;
