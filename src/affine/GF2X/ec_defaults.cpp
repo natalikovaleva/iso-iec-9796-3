94b435048f3c498cf0de6fbd58631cbebd42fdd3
avatar <avatar@IIT>
2011-02-11 18:10:52 +0200

    Add EC283, EC571

--- src/affine/GF2X/ec_defaults.cpp
+++ src/affine/GF2X/ec_defaults.cpp
@@ -16,7 +16,7 @@ using NTL::ZZ;
 
 const EC_Defaults::Sizes EC_Defaults::__size_matrix[] =
 {
-    EC0, EC163
+    EC0, EC163, EC283, EC571
 };
 
 EC_Defaults::EC_Defaults()
@@ -55,6 +55,54 @@ EC EC_Defaults::create(Sizes size)
             SEED=GF2X_str("0x0");
             break;
             
+        case EC571:
+         
+            SetCoeff(P, 571);
+            SetCoeff(P, 10);
+            SetCoeff(P, 5);
+            SetCoeff(P, 2);
+            SetCoeff(P, 0);
+
+            {
+                istringstream N_("3864537523017258344695351890931987344298927329706434998657235251451519142289560424536143999389415773083133881121926944486246872462816813070234528288303332411393191105285703");
+            
+                N_ >> N;
+            }
+            
+            
+            A  = GF2X_str("1");
+            B  = GF2X_str("2f40e7e2221f295de297117b7f3d62f5c6a97ffcb8ceff1cd6ba8ce4a9a18ad84ffabbd8efa59332be7ad6756a66e294afd185a78ff12aa520e4de739baca0c7ffeff7f2955727a");
+            
+            GX = GF2X_str("303001d34b856296c16c0d40d3cd7750a93d1d2955fa80aa5f40fc8db7b2abdbde53950f4c0d293cdd711a35b67fb1499ae60038614f1394abfa3b4c850d927e1e7769c8eec2d19");
+            GY = GF2X_str("37bf27342da639b6dccfffeb73d69d78c6c27a6009cbbca1980f8533921e8a684423e43bab08a576291af8f461bb2a8b3531d2f0485c19b16e2f1516e23dd3c1a4827af1b8ac15b");
+            
+            C  = GF2X_str("0x0");
+            SEED=GF2X_str("0x0");
+            break;
+
+        case EC283:
+            SetCoeff(P, 283);
+            SetCoeff(P, 12);
+            SetCoeff(P, 7);
+            SetCoeff(P, 5);
+            SetCoeff(P, 0);
+
+            {
+                istringstream N_("7770675568902916283677847627294075626569625924376904889109196526770044277787378692871");
+                N_ >> N;
+            }
+            
+            A  = GF2X_str("1");
+            B  = GF2X_str("27b680ac8b8596da5a4af8a19a0303fca97fd7645309fa2a581485af6263e313b79a2f5");
+            
+            GX = GF2X_str("5f939258db7dd90e1934f8c70b0dfec2eed25b8557eac9c80e2e198f8cdbecd86b12053");
+            GY = GF2X_str("3676854fe24141cb98fe6d4b20d02b4516ff702350eddb0826779c813f0df45be8112f4");
+            
+            C  = GF2X_str("0x0");
+            SEED=GF2X_str("0x0");
+            
+            break;
+
         default:
             throw;
     }
