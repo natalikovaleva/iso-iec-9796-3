42f21e5f40f82ea06a21209dbaa98fa974ee9aad
avatar <avatar@IIT>
2011-04-02 15:41:00 +0300

    ...

--- src/generic/octet.cpp
+++ src/generic/octet.cpp
@@ -100,7 +100,7 @@ std::ostream& operator<<(std::ostream& s, const ByteSeq & octet)
     std::ios_base::fmtflags original_flags = s.flags();
 
     s << std::hex;
-    
+
     for (unsigned int i=0; i<octet.__data_size; i++)
     {
         s << std::setfill('0') << std::setw(2) <<
