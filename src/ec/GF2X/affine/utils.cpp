2cb8be358fcf7bca557c11e23800bf5954b03fd6
avatar <avatar@IIT>
2011-04-02 15:38:09 +0300

    Move convertors from EC namespaces to generic one. Implement FE2OSP properly.

--- src/ec/GF2X/affine/utils.cpp
+++ src/ec/GF2X/affine/utils.cpp
@@ -1,4 +1,5 @@
 #include "ec/GF2X/affine/utils.hpp"
+#include "generic/gf2x_utils.hpp"
 
 using namespace std;
 using namespace NTL;
@@ -12,52 +13,6 @@ namespace ECGF2X
 {
     namespace Affine
     {
-
-        ByteSeq FE2OSP(unsigned int I, size_t pad)
-        {
-            const size_t size = NumBytes(I);
-            const size_t pad_need  = pad ? (size % sizeof(int)) : 0;
-            const size_t this_pad = pad_need ? sizeof(int) - pad_need : 0;
-    
-            unsigned char buffer [sizeof(int)];
-   
-            memset(buffer, 0x0, sizeof(buffer));
-
-            /* slow memcopy; LE */
-            for (unsigned int i = 0; i<size; i++)
-            {
-                buffer[sizeof(int)-i-1] = *((unsigned char *)(&I) + i);
-            }
-
-            return ByteSeq(buffer + (sizeof(int) - (size + this_pad)),
-                           size + this_pad, pad);
-        }
-
-        
-        GF2X GF2X_str(const char * source)
-        {
-            const size_t source_size = strlen(source);
-            
-            GF2X r;
-
-            for (unsigned int i=0; i<source_size; i++)
-            {
-                const unsigned char q = hexc_to_dec(source[i]);
-
-                for (unsigned int j=0; j<4; j++)
-                {
-                    if ( q & (0x8 >> j) )
-                    {
-                        SetCoeff(r, ((source_size-1 - i) * 4) + (4-j-1));
-                    }
-                }
-            }
-            
-            return r;
-        }
-
-        int tY_cnt;
-        
         ByteSeq EC2OSP(const EC_Point & Point, EC2OSP_COMPRESS_MODE mode)
         {
             const unsigned char tY =
@@ -85,50 +40,6 @@ namespace ECGF2X
                 return H || X || FE2OSP(Point.getY(), Pad);
             else
                 return H || X;
-        }
-        
-        GF2X Half_Trace(const GF2X & a, const GF2XModulus & m)
-        {
-            GF2X H = PowerMod(a % m, 4, m) + a;
-
-            /* Iteration 1 */
-
-            for (long i = 2; i<(deg(a)-1) / 2; i += 2)
-            {
-                PowerMod(H, H % m, 4, m);
-                H += a;
-            }
-
-            return H;
-        }
-
-        GF2X Z2ZB_Solve(const GF2X & b, const GF2XModulus & m)
-        {
-            /* TOOD: ADD ODD Check. If odd => use half_trace */
-            do
-            {
-                const GF2X rho = random_GF2X(deg(m)) % m;
-                GF2X z;
-                GF2X w = rho;
-
-                for (long i = 1; i < deg(m); i++)
-                {
-                    z = SqrMod(z,m) + MulMod(SqrMod(w,m), b, m);
-                    w = SqrMod(w,m) + rho;
-                }
-
-                if (! IsZero(w))
-                {
-                    // FIXME: Remove this check? Remove throw?
-                    if ( SqrMod(z,m) + z != b)
-                    {
-                        throw;
-                    }
-
-                    return z;
-                }
-
-            } while (1);
         }
     }
 }
--- src/ec/ZZ_p/affine/utils.cpp
+++ src/ec/ZZ_p/affine/utils.cpp
@@ -13,65 +13,6 @@ namespace ECZZ_p
 {
     namespace Affine
     {
-        
-        ZZ ZZ_str(const char * source)
-        {
-            const size_t source_size = strlen(source);
-            
-            ZZ r;
-
-            for (unsigned int i=0; i<source_size; i++)
-            {
-                const unsigned char q = hexc_to_dec(source[i]);
-
-                for (unsigned int j=0; j<4; j++)
-                {
-                    if ( q & (0x8 >> j) )
-                    {
-                        SetBit(r, ((source_size-1 - i) * 4) + (4-j-1));
-                    }
-                }
-            }
-            
-            return r;
-        }
-
-        ZZ_p ZZ_p_str(const char * source)
-        {
-            ZZ_p r;
-            conv(r, ZZ_str(source));
-            return r;
-        }
-        
-        
-        ByteSeq I2OSP(unsigned int I, size_t pad)
-        {
-            const size_t size = NumBytes(I);
-            const size_t pad_need  = pad ? ( size % sizeof(int) ) : 0;
-            const size_t this_pad = pad_need ? sizeof(int) - pad_need : 0;
-
-            unsigned char buffer [sizeof(int)];
-   
-            memset(buffer, 0x0, sizeof(buffer));
-
-            /* If size = 0, then return 00000000 octet */
-            
-            if (I == 0)
-            {
-                return ByteSeq(buffer, sizeof(buffer), pad);
-            }
-            
-            /* slow memcopy; LE */
-            for (unsigned int i = 0; i<size; i++)
-            {
-                buffer[sizeof(int)-i-1] = *((unsigned char *)(&I) + i);
-            }
-
-            return ByteSeq(buffer + (sizeof(int) - (size + this_pad)),
-                           ( size + this_pad ) , pad);
-        }
-
-
         ByteSeq EC2OSP(const EC_Point & Point, EC2OSP_COMPRESS_MODE mode)
         {
             const unsigned char tY = EC_CPoint::compress_tY(Point);
@@ -94,3 +35,4 @@ namespace ECZZ_p
         }
     }
 }
+
