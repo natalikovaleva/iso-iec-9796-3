1f7e5fc7dc7d12d5a6a07961d7724555b93392c9
avatar <avatar@IIT>
2011-04-02 15:38:09 +0300

    Move convertors from EC namespaces to generic one. Implement FE2OSP properly.

--- include/ec/GF2X/affine/utils.hpp
+++ include/ec/GF2X/affine/utils.hpp
@@ -5,6 +5,7 @@
 
 #include "generic/octet.hpp"
 #include "generic/convhex.h"
+#include "generic/gf2x_utils.hpp"
 #include "ec/GF2X/affine/ec.hpp"
 #include "ec/GF2X/affine/ec_compress.hpp"
 
@@ -31,63 +32,14 @@ namespace ECGF2X
 
         ByteSeq EC2OSP(const EC_Point & Point, EC2OSP_COMPRESS_MODE mode);
 
-        ByteSeq FE2OSP(unsigned I, size_t pad=0);
-
-        GF2X GF2X_str(const char * source);
+        inline ByteSeq FE2OSP(unsigned int I, size_t pad=0)
+        { return GF2X2OSP(I, pad); }
         
         inline ByteSeq FE2OSP(const GF2X & I, size_t pad=0)
-        {
-            unsigned char buffer [OCTET_MAX_SIZE];
-    
-            /* TODO: FIX BYTE ORDER */
-    
-            BytesFromGF2X(buffer, I, sizeof(buffer));
-            const size_t source_size = NumBytes(I);
-
-            return ByteSeq(buffer, source_size, pad, true);
-        }
-
+        { return GF2X2OSP(I, pad); }
+        
         inline GF2X OS2FEP(const ByteSeq & seq)
-        {
-            unsigned char buffer [OCTET_MAX_SIZE];
-    
-            const unsigned char * data = seq.getData();
-            const size_t data_size = seq.getDataSize();
-            const size_t __data_size =
-                data_size > sizeof(buffer) ?
-                sizeof(buffer) : data_size;
-
-            for (unsigned int i = 0; i<__data_size; i++)
-            {
-                buffer[i] = data[__data_size - i - 1];
-            }
-
-            return GF2XFromBytes(buffer, __data_size);
-        }
-
-        /* Length in bits */
-        inline size_t Lb(const GF2X & x)
-        { return NumBits(x); }
-
-        /* Length in bytes */
-
-        inline size_t L(const GF2X & x)
-        { return NumBytes(x); }
-
-        /* Some Number Theory functions */
-
-        GF2X Half_Trace(const GF2X & input, const GF2XModulus & m);
-        GF2X Z2ZB_Solve(const GF2X & b, const GF2XModulus & m);
-
-        /* Some primitives, that can be used
-         * TODO: Move this stuff to other header */
-
-        using ECZZ_p::Affine::ZZ_str;
-        using ECZZ_p::Affine::I2OSP;
-        using ECZZ_p::Affine::L;
-        using ECZZ_p::Affine::Lb;
-        using ECZZ_p::Affine::OS2IP;
-        using ECZZ_p::Affine::InMod;
+        { return OS2GF2XP(seq); }
     }
 }
 
--- include/ec/ZZ_p/affine/utils.hpp
+++ include/ec/ZZ_p/affine/utils.hpp
@@ -4,6 +4,7 @@
 #include <sstream>
 
 #include "generic/octet.hpp"
+#include "generic/zz_utils.hpp"
 #include "ec/ZZ_p/affine/ec.hpp"
 #include "ec/ZZ_p/affine/ec_compress.hpp"
 
@@ -13,12 +14,6 @@ namespace ECZZ_p
 {
     namespace Affine
     {
-
-        using NTL::ZZ_p;
-        using NTL::ZZ;
-        
-        ZZ_p pi(const EC_Point & X);
-
         /* Trivial faster compression */
 
         enum EC2OSP_COMPRESS_MODE
@@ -30,66 +25,12 @@ namespace ECZZ_p
 
         ByteSeq EC2OSP(const EC_Point & Point,
                        EC2OSP_COMPRESS_MODE mode = EC2OSP_COMPRESSED);
+        
+        inline ByteSeq FE2OSP(const ZZ_p & I, size_t pad=0)
+        { return I2OSP(I, pad); }
 
-        ByteSeq I2OSP(unsigned I, size_t pad=0);
-
-        ZZ_p ZZ_p_str(const char * source);
-        ZZ ZZ_str(const char * source);
-
-        inline ByteSeq I2OSP(const ZZ & I, size_t pad=0)
-        {
-            unsigned char buffer [OCTET_MAX_SIZE];
-    
-            /* TODO: FIX BYTE ORDER */
-            
-            BytesFromZZ(buffer, I, sizeof(buffer));
-            const size_t source_size = NumBytes(I);
-
-            return ByteSeq(buffer, source_size, pad, true);
-        }
-
-        inline ByteSeq I2OSP(const ZZ_p & I, size_t pad=0)
-        {
-            return I2OSP(rep(I), pad);
-        }
-
-        inline ZZ OS2IP(const ByteSeq & seq)
-        {
-            unsigned char buffer [OCTET_MAX_SIZE];
-    
-            const unsigned char * data = seq.getData();
-            const size_t data_size = seq.getDataSize();
-            const size_t __data_size =
-                data_size > sizeof(buffer) ?
-                sizeof(buffer) : data_size;
-
-            for (unsigned int i = 0; i<__data_size; i++)
-            {
-                buffer[i] = data[__data_size - i - 1];
-            }
-
-            return ZZFromBytes(buffer, __data_size);
-        }
-
-        /* Length in bits */
-        inline size_t Lb(const ZZ & x)
-        { return NumBits(x); }
-
-        inline size_t Lb(const ZZ_p & x)
-        { return NumBits(rep(x)); }
-
-        /* Length in bytes */
-
-        inline size_t L(const ZZ & x)
-        { return NumBytes(x); }
-
-        inline size_t L(const ZZ_p & x)
-        { return NumBytes(rep(x)); }
-
-        inline ZZ_p InMod(const ZZ & x)
-        {
-            ZZ_p r; conv(r,x); return r;
-        }
-
+        inline ZZ_p OS2FEP(const ByteSeq & seq)
+        { return InMod(OS2IP(seq)); }
+        
     }
 }
