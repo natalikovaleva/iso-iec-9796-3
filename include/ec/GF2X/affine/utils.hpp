bd0ea4b92d221b57da47b593d8e332b6f0bb721c
Alexey Shevchuck <avatar@love-n-peace.if.ua>
2011-04-23 19:47:04 +0300

    Add new FE2IP utilitary function to direct FE->I convertions

--- include/ec/GF2X/affine/utils.hpp
+++ include/ec/GF2X/affine/utils.hpp
@@ -6,6 +6,7 @@
 #include "generic/octet.hpp"
 #include "generic/convhex.h"
 #include "generic/gf2x_utils.hpp"
+#include "generic/zz_utils.hpp"
 #include "ec/GF2X/affine/ec.hpp"
 #include "ec/GF2X/affine/ec_compress.hpp"
 
@@ -18,7 +19,7 @@ namespace ECGF2X
     namespace Affine
     {
         using NTL::GF2X;
-        
+
         GF2X pi(const EC_Point & X);
 
         /* Trivial faster compression */
@@ -27,10 +28,13 @@ namespace ECGF2X
 
         inline ByteSeq FE2OSP(unsigned int I, size_t pad=0)
         { return GF2X2OSP(I, pad); }
-        
+
         inline ByteSeq FE2OSP(const GF2X & I, size_t pad=0)
         { return GF2X2OSP(I, pad); }
-        
+
+        inline ZZ FE2IP(const GF2X & I)
+        { return OS2IP(FE2OSP(I)); }
+
         inline GF2X OS2FEP(const ByteSeq & seq)
         { return OS2GF2XP(seq); }
     }
