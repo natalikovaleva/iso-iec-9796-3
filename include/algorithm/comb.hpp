b146b42eed1c1c9b09533bf43318446f82d7a497
Alexey Shevchuck <avatar@love-n-peace.if.ua>
2012-10-08 09:22:18 +0300

    Remove whitespaces

--- include/algorithm/comb.hpp
+++ include/algorithm/comb.hpp
@@ -6,25 +6,25 @@
 
 namespace Algorithm
 {
-    
+
     template <class Group1,
               class Group2,
               class PGroup1>
-    
+
     class Precomputations_Comb : public Precomputations_Logic <Group1,
                                                                Group2>
     {
         const long __precomputations_window;
         const long __precomputations_elements;
-            
+
         PGroup1 ** __precomputations;
-            
+
         const long __precomputations_max_size;
         const long __precomputations_portion;
 
     private:
         Precomputations_Comb(Precomputations_Comb & Source);
-        
+
     public:
         Precomputations_Comb()
             : __precomputations_window(0),
@@ -33,7 +33,7 @@ namespace Algorithm
               __precomputations_max_size(0),
               __precomputations_portion(0)
             {} /* Empty precomputations. Totaly useless */
-        
+
         Precomputations_Comb(const Group1 & Source,
                                    size_t MaxMulSize)
             : __precomputations_window(comb_window),
@@ -45,7 +45,7 @@ namespace Algorithm
               __precomputations_portion(__precomputations_max_size / __precomputations_window)
             {
                 conv1<PGroup1, Group1> groupConvertor;
-                
+
                 PGroup1 sPoint(groupConvertor(Source)); // Do something here?
 
                 /* Fill 0, 1 */
@@ -62,10 +62,10 @@ namespace Algorithm
                         new PGroup1(
                             wdouble(*prev,
                                     __precomputations_portion));
-        
+
                     prev = __precomputations[1<<i];
                 }
-    
+
                 for (long i = 0; i<__precomputations_elements - __precomputations_window - 1; i++)
                 {
                     long t_idx = comb_idx[i*3];
@@ -77,19 +77,19 @@ namespace Algorithm
                 }
             }
 
-            
+
         ~Precomputations_Comb()
             {
                 for (long i = 0; i< __precomputations_elements; i++)
                 {
                     delete __precomputations[i];
-        
+
                     __precomputations[i] = NULL;
                 }
-                
+
                 delete [] __precomputations;
             }
-        
+
 
     public:
         void Multiply(      Group1 & P,
@@ -105,15 +105,15 @@ namespace Algorithm
                     P += getPrecomputedForMul(Y, i);
                 }
             }
-        
-        inline bool isReady(void) 
+
+        inline bool isReady(void)
             { return true; }
-            
-            
+
+
     private:
         long getMulPortions(void) const
             { return __precomputations_portion; }
-        
+
         const PGroup1 & getPrecomputedForMul(const Group2 & scalar,
                                              const long portion) const
             {
@@ -129,15 +129,15 @@ namespace Algorithm
             }
 
         /* ---- REALIZATION DETAILS ---- */
-        
+
         static inline PGroup1 wdouble(const PGroup1 & input,
                                       long window)
             {
                 PGroup1 r(input);
-                
+
                 for (long i=0; i<window; i++)
                     r += r;
-                
+
                 return r;
             }
 
@@ -147,7 +147,7 @@ namespace Algorithm
                                             const long iteration)
             {
                 long index = 0x0;
-                
+
                 for (long i = 0; i<window; i++)
                 {
                     index |=
@@ -166,19 +166,19 @@ namespace Algorithm
     public:
         Precomputations_Method_Comb(size_t MulSize)
             : Precomputations_Method<Group1, Group2>(MulSize) {};
-        
+
     public:
         Precomputations<Group1, Group2>
         operator()(const Group2 & arg2 __attribute__ ((unused))) const
             __attribute__ ((noreturn))
             { throw; } // NOT IMPLEMENTED
-        
+
         Precomputations<Group1, Group2>
         operator()(const Group1 & arg1 __attribute__ ((unused)),
                    const Group2 & arg2 __attribute__ ((unused))) const
             __attribute__ ((noreturn))
             { throw; } // NOT IMPLEMENTED
-        
+
         Precomputations<Group1, Group2>
         operator()(const Group1 & arg1) const
             { Precomputations<Group1,
